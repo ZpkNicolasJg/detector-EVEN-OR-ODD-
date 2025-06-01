93f435ebb4078b5847a550bb28489bcc4328eaa6
Nicolas guedes <nicolasguedesjava@gmail.com>
2025-05-31 21:39:09 -0300

    detector: EVEN OR ODD

---
+++ etector.py
@@ -0,0 +1,8 @@
+# Recebe um número do usuário / Get a number from the user
+n = int(input("Me fale um número para eu detectar se é PAR OU ÍMPAR! "))
+
+# Verifica se o número é par / Check if the number is even
+if n % 2 == 0:
+    print("O número é PAR!")
+else:
+    print("O número é ÍMPAR!")
